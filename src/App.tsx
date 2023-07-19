161a85d5d27f7bd75aba8e71426cba29813e0d0c
Rafael Pelle <rafapelle@gmail.com>
2023-07-19 15:05:49 -0300

    add image credits

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import { Fragment } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ResultsPage from './pages/Results';
+import Credits from './components/Credits';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/results" element={<ResultsPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/results" element={<ResultsPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+
+      <Credits />
+    </Fragment>
   );
 }
 
---
+++ src/components/Credits/index.tsx
@@ -0,0 +1,25 @@
+import { Box } from '@mui/material';
+import { styled } from 'styled-components';
+
+const CustomBox = styled(Box)`
+  position: absolute;
+  bottom: 10px;
+  left: 0;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
+
+function Credits() {
+  return (
+    <CustomBox>
+      <small>
+        <a href="https://www.flaticon.com/free-icons/route" title="route icons">
+          Route icon created by Freepik - Flaticon
+        </a>
+      </small>
+    </CustomBox>
+  );
+}
+
+export default Credits;
